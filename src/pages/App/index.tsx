f8172b523e56fcb6089baf34c13c6a79857b2645
Tomás Leggire <tomasleggire@hotmail.com>
2023-10-11 19:47:48 +0000

    useRoutes implemented

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../Home";
 import "./App.css";
 
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "*", element: <p>Not Found</p> },
+  ]);
+  return routes;
+};
+
 function App() {
   return (
-    <div className="App">
-      App
-      <Home />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
